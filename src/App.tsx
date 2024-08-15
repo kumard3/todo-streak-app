1f887a7b7ec21c162ded537c1f9ed0b1e7dfc6bd
kumard3 <kumardeepanshu157200@gmail.com>
2024-08-15 23:31:32 +0530

    refactor: Rearrange components in App.tsx for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -79,18 +79,14 @@ export const App: React.FC = () => {
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Habit Tracker</h1>
       <div className="flex w-full justify-between">
-        <div>
+        <div className="flex gap-5">
           {/* AddTodo component for adding new todos */}
           <AddTodo onAdd={addTodo} />
-          {/* DateChanger component for changing the current date */}
-          <DateChanger
-            currentDate={currentDate}
-            onDateChange={setCurrentDate}
-          />
+          {/* Backtracking component for displaying and handling backtracking */}
+          <Backtracking todos={todos} onBacktrack={handleBacktrack} />
         </div>
-
-        {/* Backtracking component for displaying and handling backtracking */}
-        <Backtracking todos={todos} onBacktrack={handleBacktrack} />
+        {/* DateChanger component for changing the current date */}
+        <DateChanger currentDate={currentDate} onDateChange={setCurrentDate} />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {/* TodoList component for displaying daily habits */}
